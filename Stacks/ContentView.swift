02f91fbc076921e5d8f335c53a82de6dabe8d6b7
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-01-14 13:12:37 +0200

    init

--- Stacks/ContentView.swift
+++ Stacks/ContentView.swift
@@ -10,7 +10,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            
+            HStack {
+                Image(systemName: "house")
+                Text("Welcome Home!")
+            }
+            
+            ZStack {
+                Circle()
+                    .foregroundColor(.blue)
+                    .frame(width: 100, height: 100)
+                
+                Text("Dor")
+                    .foregroundColor(.white)
+            }
+        }
     }
 }
 
